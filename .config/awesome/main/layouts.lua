f6122f8ef5dbfc6c5b638ee68da709f6530afcc4
Syed Dawar Alvi <46403138+DawarAlvi@users.noreply.github.com>
2023-12-25 12:22:14 +0530

    awesome wm layouts

--- .config/awesome/main/layouts.lua
+++ .config/awesome/main/layouts.lua
@@ -4,9 +4,8 @@ local _M = {}
 
 function _M.get()
     local layouts = {
-        awful.layout.suit.spiral.dwindle,
-        awful.layout.suit.floating,
         awful.layout.suit.max,
+        awful.layout.suit.spiral.dwindle,
     }
 
     return layouts
